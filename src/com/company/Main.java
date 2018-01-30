108859ea611025367318acc9899fddc6d4bb0f1b
Tlenc <tlenciauskas@gmail.com>
2018-01-30 14:34:05 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-    /**/
+        System.out.println("Įveskite savo vardą");
+        Scanner sc = new Scanner(System.in);
+                String vardas = sc.nextLine();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+
     }
 }
